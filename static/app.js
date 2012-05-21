0d77f3b90595d4e7fd464b456ce3f58a613050a3
Mark Fayngersh <phunny.phacts@gmail.com>
2012-05-21 13:49:43 -0400

    working on views

--- static/app.js
+++ static/app.js
@@ -1,8 +1,26 @@
-var MainView = Backbone.View.extend({
-  id: 'main',
+views = {}
+
+views.SectionView = Backbone.View.extend({
+  
+});
+
+views.StreamView = Backbone.View.extend({
+  
+});
+
+views.MainView = Backbone.View.extend({
+  id: 'jqt',
+  
+  initialize: function(conn) {
+    views.conn = conn;
+    this.section = new views.SectionView();
+    this.stream = new views.StreamView();
+  },
+  
+  template: _.template("<div id='section' class='current'>section</div><div id='stream' class='current'>stream</div>"),
   
   render: function() {
-    this.$el.html("<h1>TEST</h1>");
+    this.$el.html(this.template());
     return this;
   }
 });
