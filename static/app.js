6e18659b5a4f4c08ed0c558780a9e80f344710a0
Mark Fayngersh <phunny.phacts@gmail.com>
2012-05-21 18:55:26 -0400

    changes

--- static/app.js
+++ static/app.js
@@ -3,19 +3,33 @@ views = {}
 views.SectionView = Backbone.View.extend({  
   id: 'section',
   
-  template: _.template("<div id='status'>status</div><div id='events-list'></div><input id='msg' type='text' placeholder='Enter message'>"),
+  template: _.template("<div id='status'><h1></h1><h2>2 people</h2></div> \
+  <div id='section-select'> \
+    <label for='search'>Section select</label> \
+    <input id='search' type='text' placeholder='Section name'> \
+  </div><div style='display:none;' id='events-list'></div><form style='display:none;'><input id='msg' type='text' placeholder='Enter message'></form>"),
   
   events: {
     'submit form':'post'
-  }
+  },
   
   initialize: function(params) {
-    
+    conn.on('message.join-ok', function() {
+      this.$('h1').text(conn.sectionName);
+    }, this);
+    views.conn.sectionEvents.on('add', function(m,c) {
+     var new_chat = (m instanceof model.Chat) ? new views.ChatView({model:m}) : new views.PollView({model:m});
+     this.$('#events-list').append(new_chat.render().el);
+     this.$('.chat-event:last').fadeIn('fast');
+     this.$('#events-list').scrollTop(Math.pow(2,30));
+    }, this);
   },
   
   post: function(event) {
-    
-  }
+    event.preventDefault();
+    views.conn.chat($('#msg').val());
+    $('#msg').val('').focus();
+  },
   
   render: function() {
     this.$el.html(this.template());
@@ -26,10 +40,21 @@ views.SectionView = Backbone.View.extend({
 views.ChatView = Backbone.View.extend({
   className: 'chat-event',
   
-  _.template("<img class='profile' src='/static/img/users/<%= user %>' /> <p class='text'><%= text %></p>"),
+  template:  _.template("<img class='profile' src='<%=avatarUrl%>' /><p class='text'><%=message%></p>"),
   
   render: function() {
-    
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+views.PollView = Backbone.View.extend({
+  className: 'poll-event',
+  
+  template: _.template(""),
+  
+  render: function() {
+    this.$el.html(this.template(this.model));
   }
 });
 
