1a23ef4099a9105ffb1456eae5b1fefd1be4e443
Mark Fayngersh <phunny.phacts@gmail.com>
2012-05-21 16:33:49 -0400

    view stuff

--- static/app.js
+++ static/app.js
@@ -1,11 +1,47 @@
 views = {}
 
-views.SectionView = Backbone.View.extend({
+views.SectionView = Backbone.View.extend({  
+  id: 'section',
   
+  template: _.template("<div id='status'>status</div><div id='events-list'></div><input id='msg' type='text' placeholder='Enter message'>"),
+  
+  events: {
+    'submit form':'post'
+  }
+  
+  initialize: function(params) {
+    
+  },
+  
+  post: function(event) {
+    
+  }
+  
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
+});
+
+views.ChatView = Backbone.View.extend({
+  className: 'chat-event',
+  
+  _.template("<img class='profile' src='/static/img/users/<%= user %>' /> <p class='text'><%= text %></p>"),
+  
+  render: function() {
+    
+  }
 });
 
 views.StreamView = Backbone.View.extend({
+  id: 'stream',
   
+  template: _.template("stuff"),
+  
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
 });
 
 views.MainView = Backbone.View.extend({
@@ -17,10 +53,13 @@ views.MainView = Backbone.View.extend({
     this.stream = new views.StreamView();
   },
   
-  template: _.template("<div id='section'>section</div><div id='stream'>stream</div>"),
+  template: _.template(""),
   
   render: function() {
     this.$el.html(this.template());
+    this.$el.append(this.section.render().el);
+    this.$el.append(this.stream.render().el)
+    
     return this;
   }
 });
